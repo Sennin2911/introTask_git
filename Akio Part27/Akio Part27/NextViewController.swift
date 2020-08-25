62b146245c8acc6ba86e79b321fc43c11246eb93
Sennin2911 <sennin2911@gmail.com>
2020-08-25 22:26:22 +0900

    initial commit

---
+++ Akio Part27/Akio Part27/NextViewController.swift
@@ -0,0 +1,33 @@
+//
+//  NextViewController.swift
+//  Akio Part27
+//
+//  Created by 蓑星友浩 on 2020/08/25.
+//  Copyright © 2020 Sennin. All rights reserved.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+
+    @IBOutlet weak var Label2: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Akio Part27/Akio Part27/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Akio Part27
+//
+//  Created by 蓑星友浩 on 2020/08/25.
+//  Copyright © 2020 Sennin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var button1: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+   
+}
+
